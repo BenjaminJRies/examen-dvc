c0ac13152632ae1d604e44a900fdbc368997166d
BenjaminJRies <benjaminjacobries@gmail.com>
2025-06-11 22:17:18 +0000

    corrected requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project has been developed from a starting Pack for MLOps project. It s not perfect so feel free to make some modifications on it.',
+    author='Benjamin Ries',
+    license='MIT',
+)
